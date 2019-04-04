28d1c30cc4657eff436fd6725c1a9aadcb573fab
Tim <notquitedrdeath@gmail.com>
2019-04-04 23:54:09 +1100

    Moves in Characters

--- characters/character.py
+++ characters/character.py
@@ -4,14 +4,34 @@ import os
 CHAR_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 
-class Character(object):
+class JSONSerialisableObject(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def serialise(self):
+        json_dict = {}
+        for item in self.__dict__:
+            if not item.startswith('__') and not callable(item):
+                json_dict[item] = self.__dict__[item]
+        return json.dumps(json_dict, indent=4, default=str)
+
+    def __str__(self):
+        return self.serialise()
+
+    @classmethod
+    def deserialise(cls, json_str):
+        return cls(**json.loads(json_str))
+
+
+class Character(JSONSerialisableObject):
     def __init__(self, name='Peter',
                  health=1000, stun=1000,
                  walkspeed_forward=3, walkspeed_back=3,
                  dash_forward=17, dash_back=17,
                  jump_forward_speed=(3, 38, 4), jump_neutral_speed=(4, 38, 4), jump_back_speed=(4, 38, 4),
                  jump_forward_distance=150, jump_back_distance=150,
-                 movelist=None, knockdowns=None, *args, **kwags):
+                 movelist=None, knockdowns=None, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
         self.name = name
         self.health = health
         self.stun = stun
@@ -28,24 +48,10 @@ class Character(object):
         self.movelist = movelist or []
         self.knockdowns = knockdowns or []
 
-    def serialise(self):
-        json_dict = {}
-        for item in self.__dict__:
-            if not item.startswith('__') and not callable(item):
-                json_dict[item] = self.__dict__[item]
-        return json.dumps(json_dict, indent=4)
-
-    def __str__(self):
-        return self.serialise()
-
-    @classmethod
-    def deserialise(cls, json_str):
-        return cls(**json.loads(json_str))
-
     def save(self, location=None):
         location = location or os.path.join(CHAR_DIRECTORY, "{}.json".format(self.name))
         with open(location, 'w') as save_file:
-            save_file.write(self.serialise())
+            save_file.write(str(self))
 
     @classmethod
     def load(cls, char_name=None, location=None):
@@ -54,12 +60,16 @@ class Character(object):
             character = cls.deserialise(load_file.read())
         return character
 
-class Move(object):
+    def add_move(self, move):
+        self.movelist.append(move)
+
+class Move(JSONSerialisableObject):
     def __init__(self, name, input="s.LP",
                  startup=3, active=2, recovery=7,
                  on_block=2, on_hit=4, on_ch=None,
-                 max_range=0.6, pushback_block=0.5, pushback_hit=0.4, pushback_ch=None,
+                 max_range=0.6, pushback_block=0.5, pushback_hit=0.4, pushback_ch=None, can_cc=False,
                  damage=30, stun=70, cancels=None, comments=None, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
         self.name = name
         self.input = input
         self.startup = startup
@@ -74,11 +84,21 @@ class Move(object):
             self.active = active
         self.recovery = recovery
         self.total_frames = self.startup + self.recovery + sum([pair[0] + pair[1] for pair in self.active]) - 1
-        self.on_block= on_block
+        self.on_block = on_block
         self.on_hit = on_hit
         # If there's an explicit CH value given, assume that it's a CC value, otherwise just use oH + 2
         self.on_ch = on_ch or on_hit + 2
-
+        self.damage = damage
+        self.stun = stun
+        # No idea how to store this information.
+        self.cancels = cancels
+        self.max_range = max_range
+        self.pushback_block = pushback_block
+        self.pushback_hit = pushback_hit
+        self.pushback_ch = pushback_ch
+        # Boolean to check if the move will CC
+        self.can_cc = can_cc
+        self.comments = comments
 
     def move_string(self):
         move_str = (self.startup-1) * "s"
@@ -86,3 +106,6 @@ class Move(object):
             move_str += gap * "r" + active * "A"
         move_str += self.recovery * "r"
         return move_str
+
+    def __str__(self):
+        return self.serialise()
